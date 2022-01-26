9460d3baa30c7d379349e3139fc66b05507d920b
skrachamalla <skrachamalla@gmail.com>
2022-01-26 18:42:09 +0530

    file3.sh has been created aiming to do addition of two numbers

---
+++ file3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# A program used to test the github, wheather we can push and pull the code or not.
+# To understand it better I'm writing a simple case statement
+# If any changes then, correct it.
+function addition()
+{
+	echo "=================================="
+	$a
+	echo "----------------------------------"
+	$b
+	echo "----------------------------------"
+}
+	addition`$a+$b`
+
+	Result
+	echo " The sum of $a and $b is $[ $a+$b ]
+	echo "=================================="
